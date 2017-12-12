3e9cdb5db0fa8109c5bdffa854cab08d6a63c4ef
曹冠雄 <1120643432@qq.com>
2017-12-12 16:36:59 +0800

    Update. Update UITableViewCell+CGXExtension.

--- CGXExtension/CGXUIKitExtension/UITableViewCell+CGXExtension.h
+++ CGXExtension/CGXUIKitExtension/UITableViewCell+CGXExtension.h
@@ -10,4 +10,6 @@
 
 @interface UITableViewCell (CGXExtension)
 
+- (CGFloat)autoLayoutHeight;
+
 @end
--- CGXExtension/CGXUIKitExtension/UITableViewCell+CGXExtension.m
+++ CGXExtension/CGXUIKitExtension/UITableViewCell+CGXExtension.m
@@ -10,4 +10,18 @@
 
 @implementation UITableViewCell (CGXExtension)
 
+- (CGFloat)autoLayoutHeight {
+    /**
+     由于是contentView调用，返回的是contentView的高度
+     cell高度比contentView高度多1，也就是分割线的高度
+     所以需要加上1才是最终cell的高度
+     */
+    return [self.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height + 1;
+    /**
+     如果cell中使用了UITextView
+     由于systemLayoutSizeFittingSize并没有把textView的高度计算在内
+     所以需要单独计算textView的高度，并加上contentView的高度
+     */
+}
+
 @end
