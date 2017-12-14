aaa868c0cf6cdf53b3825d1b3d6b10d601ac970a
曹冠雄 <1120643432@qq.com>
2017-12-14 18:23:50 +0800

    Update. Update UIViewController+CGXExtension with alertController method.

--- CGXExtension/CGXUIKitExtension/UIViewController+CGXExtension.h
+++ CGXExtension/CGXUIKitExtension/UIViewController+CGXExtension.h
@@ -8,6 +8,122 @@
 
 #import <UIKit/UIKit.h>
 
-@interface UIViewController (CGXExtension)
+//https://github.com/kukumaluCN/JXTAlertManager
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark - CGXAlertController.h
+
+@class CGXAlertController;
+/**
+ CGXAlertController: alertAction配置链
+ 
+ @param title 标题
+ @return      CGXAlertController对象
+ */
+typedef CGXAlertController * _Nonnull (^CGXAlertActionTitle)(NSString *title);
+
+/**
+ CGXAlertController: alert按钮执行回调
+ 
+ @param buttonIndex 按钮index(根据添加action的顺序)
+ @param action      UIAlertAction对象
+ @param alertSelf   本类对象
+ */
+typedef void (^CGXAlertActionBlock)(NSInteger buttonIndex, UIAlertAction *action, CGXAlertController *alertSelf);
+
+
+/**
+ CGXAlertController 简介：
+ 
+ 1.针对系统UIAlertController封装，支持iOS8及以上
+ 
+ 2.关于iOS9之后的`preferredAction`属性用法:
+ `alertController.preferredAction = alertController.actions[0];`
+ 效果为将已存在的某个action字体加粗，原cancel样式的加粗字体成为deafult样式，cancel样式的action仍然排列在最下
+ 总体意义不大，且仅限于`UIAlertControllerStyleAlert`，actionSheet无效，功能略微鸡肋，不再单独封装
+ 
+ 3.关于`addTextFieldWithConfigurationHandler:`方法:
+ 该方法同样仅限于`UIAlertControllerStyleAlert`使用，使用场景较为局限，推荐直接调用，不再针对封装
+ 
+ 4.关于自定义按钮字体或者颜色，可以利用kvc间接访问这些私有属性，但是不推荐
+ `[alertAction setValue:[UIColor grayColor] forKey:@"titleTextColor"]`
+ */
+NS_CLASS_AVAILABLE_IOS(8_0) @interface CGXAlertController : UIAlertController
+
+
+/**
+ CGXAlertController: 禁用alert弹出动画，默认执行系统的默认弹出动画
+ */
+- (void)alertAnimateDisabled;
+
+/**
+ CGXAlertController: alert弹出后，可配置的回调
+ */
+@property (nullable, nonatomic, copy) void (^alertDidShown)(void);
+
+/**
+ CGXAlertController: alert关闭后，可配置的回调
+ */
+@property (nullable, nonatomic, copy) void (^alertDidDismiss)(void);
+
+/**
+ CGXAlertController: 设置toast模式展示时间：如果alert未添加任何按钮，将会以toast样式展示，这里设置展示时间，默认1s
+ */
+@property (nonatomic, assign) NSTimeInterval toastStyleDuration; //deafult CGX_alertShowDurationDefault = 1s
+
+
+/**
+ CGXAlertController: 链式构造alert视图按钮，添加一个alertAction按钮，默认样式，参数为标题
+ 
+ @return CGXAlertController对象
+ */
+- (CGXAlertActionTitle)addActionDefaultTitle;
+
+/**
+ CGXAlertController: 链式构造alert视图按钮，添加一个alertAction按钮，取消样式，参数为标题(warning:一个alert该样式只能添加一次!!!)
+ 
+ @return CGXAlertController对象
+ */
+- (CGXAlertActionTitle)addActionCancelTitle;
+
+/**
+ CGXAlertController: 链式构造alert视图按钮，添加一个alertAction按钮，警告样式，参数为标题
+ 
+ @return CGXAlertController对象
+ */
+- (CGXAlertActionTitle)addActionDestructiveTitle;
 
 @end
+
+#pragma mark - UIViewController + CGXExtension.h
+
+/**
+ CGXAlertController: alert构造块
+ 
+ @param alertMaker CGXAlertController配置对象
+ */
+typedef void(^CGXAlertButtonsConfig)(CGXAlertController *alertMaker);
+
+typedef void (^AlertActionBlock)(NSInteger buttonIndex, UIAlertAction *action);
+
+@interface UIViewController (CGXExtension)
+
+/**
+ 弹出 UIAlertController
+
+ @param style alert 样式还是 actiuonSheet 样式
+ @param title 标题
+ @param message 内容
+ @param buttonsConfig 配置按钮
+ @param actionBlock 按钮点击回调
+ */
+- (void)showAlertWithStyle:(UIAlertControllerStyle)style
+                     title:(nullable NSString *)title
+                   message:(nullable NSString *)message
+             buttonsConfig:(CGXAlertButtonsConfig)buttonsConfig
+              actionsBlock:(nullable CGXAlertActionBlock)actionBlock NS_AVAILABLE_IOS(8_0);
+
+@end
+
+NS_ASSUME_NONNULL_END
--- CGXExtension/CGXUIKitExtension/UIViewController+CGXExtension.m
+++ CGXExtension/CGXUIKitExtension/UIViewController+CGXExtension.m
@@ -8,6 +8,185 @@
 
 #import "UIViewController+CGXExtension.h"
 
+//toast默认展示时间, 可通过设置 alertMaker.toastStyleDuration来自定义
+static NSTimeInterval const CGXAlertShowDurationDefault = 1.0f;
+
+#pragma mark - AlertActionModel.h
+
+@interface CGXAlertActionModel : NSObject
+
+@property (nonatomic, copy) NSString * title;
+@property (nonatomic, assign) UIAlertActionStyle style;
+
+@end
+
+#pragma mark - AlertActionModel.m
+
+@implementation CGXAlertActionModel
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.title = @"";
+        self.style = UIAlertActionStyleDefault;
+    }
+    return self;
+}
+
+@end
+
+#pragma mark - CGXAlertController.h
+/**
+ AlertActions配置
+ 
+ @param actionBlock CGXAlertActionBlock
+ */
+typedef void (^CGXAlertActionsConfig)(CGXAlertActionBlock actionBlock);
+
+
+@interface CGXAlertController ()
+
+//CGXAlertActionModel数组
+@property (nonatomic, strong) NSMutableArray<CGXAlertActionModel *> *alertActionArray;
+//是否操作动画
+@property (nonatomic, assign) BOOL setAlertAnimated;
+//action配置
+- (CGXAlertActionsConfig)alertActionsConfig;
+
+@end
+
+#pragma mark - CGXAlertController.m
+
+@implementation CGXAlertController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    if (self.alertDidDismiss) {
+        self.alertDidDismiss();
+    }
+}
+
+#pragma mark - Private
+
+//action-title数组
+- (NSMutableArray<CGXAlertActionModel *> *)alertActionArray {
+    if (_alertActionArray == nil) {
+        _alertActionArray = [NSMutableArray array];
+    }
+    return _alertActionArray;
+}
+
+//action配置
+- (CGXAlertActionsConfig)alertActionsConfig {
+    
+    return ^(CGXAlertActionBlock actionBlock) {
+        if (self.alertActionArray.count > 0) {
+            //创建action
+            __weak typeof(self)weakSelf = self;
+            [self.alertActionArray enumerateObjectsUsingBlock:^(CGXAlertActionModel *actionModel, NSUInteger idx, BOOL * _Nonnull stop) {
+                UIAlertAction *alertAction = [UIAlertAction actionWithTitle:actionModel.title style:actionModel.style handler:^(UIAlertAction * _Nonnull action) {
+                    __strong typeof(weakSelf)strongSelf = weakSelf;
+                    if (actionBlock) {
+                        actionBlock(idx, action, strongSelf);
+                    }
+                }];
+                //可利用这个改变字体颜色，但是不推荐！！！
+                //[alertAction setValue:[UIColor grayColor] forKey:@"titleTextColor"];
+                //action作为self元素，其block实现如果引用本类指针，会造成循环引用
+                [self addAction:alertAction];
+            }];
+        } else {
+            NSTimeInterval duration = self.toastStyleDuration > 0 ? self.toastStyleDuration : CGXAlertShowDurationDefault;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(duration * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [self dismissViewControllerAnimated:!(self.setAlertAnimated) completion:NULL];
+            });
+        }
+    };
+}
+
+#pragma mark - Public
+
+- (instancetype)initAlertControllerWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)preferredStyle {
+    
+    if (!(title.length > 0) && (message.length > 0) && (preferredStyle == UIAlertControllerStyleAlert)) {
+        title = @"";
+    }
+    self = [[self class] alertControllerWithTitle:title message:message preferredStyle:preferredStyle];
+    if (!self) return nil;
+    
+    self.setAlertAnimated = NO;
+    self.toastStyleDuration = CGXAlertShowDurationDefault;
+    
+    return self;
+}
+
+- (void)alertAnimateDisabled {
+    self.setAlertAnimated = YES;
+}
+
+- (CGXAlertActionTitle)addActionDefaultTitle {
+    //该block返回值不是本类属性，只是局部变量，不会造成循环引用
+    return ^(NSString *title) {
+        CGXAlertActionModel *actionModel = [[CGXAlertActionModel alloc] init];
+        actionModel.title = title;
+        actionModel.style = UIAlertActionStyleDefault;
+        [self.alertActionArray addObject:actionModel];
+        return self;
+    };
+}
+
+- (CGXAlertActionTitle)addActionCancelTitle {
+    return ^(NSString *title) {
+        CGXAlertActionModel *actionModel = [[CGXAlertActionModel alloc] init];
+        actionModel.title = title;
+        actionModel.style = UIAlertActionStyleCancel;
+        [self.alertActionArray addObject:actionModel];
+        return self;
+    };
+}
+
+- (CGXAlertActionTitle)addActionDestructiveTitle {
+    return ^(NSString *title) {
+        CGXAlertActionModel *actionModel = [[CGXAlertActionModel alloc] init];
+        actionModel.title = title;
+        actionModel.style = UIAlertActionStyleDestructive;
+        [self.alertActionArray addObject:actionModel];
+        return self;
+    };
+}
+
+@end
+
+#pragma mark - UIViewController + CGXExtension.m
+
 @implementation UIViewController (CGXExtension)
+
+- (void)showAlertWithStyle:(UIAlertControllerStyle)style title:(NSString *)title message:(NSString *)message buttonsConfig:(CGXAlertButtonsConfig)buttonsConfig actionsBlock:(CGXAlertActionBlock)actionBlock {
+    
+    if (buttonsConfig) {
+        CGXAlertController *alertMaker = [[CGXAlertController alloc] initAlertControllerWithTitle:title message:message preferredStyle:style];
+        //防止nil
+        if (!alertMaker) {
+            return ;
+        }
+        //加工链
+        buttonsConfig(alertMaker);
+        //配置响应
+        alertMaker.alertActionsConfig(actionBlock);
+        
+        if (alertMaker.alertDidShown) {
+            [self presentViewController:alertMaker animated:!(alertMaker.setAlertAnimated) completion:^{
+                alertMaker.alertDidShown();
+            }];
+        } else {
+            [self presentViewController:alertMaker animated:!(alertMaker.setAlertAnimated) completion:nil];
+        }
+    }
+}
 
 @end
