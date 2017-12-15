4bf7540214042f3ca3bb36c9a0168af83dff4ebc
曹冠雄 <1120643432@qq.com>
2017-12-15 14:30:18 +0800

    Update. Update UIImageView+CGXExtension.

--- CGXExtension/CGXUIKitExtension/UIImageView+CGXExtension.h
+++ CGXExtension/CGXUIKitExtension/UIImageView+CGXExtension.h
@@ -10,4 +10,20 @@
 
 @interface UIImageView (CGXExtension)
 
+- (void)setImageWithName:(NSString *)name;
+
+- (void)setImageWithURL:(NSString *)url;
+- (void)setImageWithURL:(NSString *)url placeholderImage:(NSString *)placeholderImage;
+- (void)setImageWithURL:(NSString *)url placeholderImage:(NSString *)placeholderImage completion:(void (^)(UIImage *image, NSError *error, NSURL *imageURL))completion;
+
+- (void)startAnimatingWithGifImageName:(NSString *)gifImageName;
+- (void)startAnimatingWithGifImageName:(NSString *)gifImageName repeatCount:(NSUInteger)repeatCount;
+
+- (void)startAnimatingWithGifImagePath:(NSString *)gifImagePath;
+- (void)startAnimatingWithGifImagePath:(NSString *)gifImagePath repeatCount:(NSUInteger)repeatCount;
+
+- (void)startAnimatingWithImages:(NSArray<UIImage *> *)images duration:(CGFloat)duration;
+- (void)startAnimatingWithImages:(NSArray<UIImage *> *)images duration:(CGFloat)duration repeatCount:(NSUInteger)repeatCount;
+
 @end
+
--- CGXExtension/CGXUIKitExtension/UIImageView+CGXExtension.m
+++ CGXExtension/CGXUIKitExtension/UIImageView+CGXExtension.m
@@ -7,7 +7,63 @@
 //
 
 #import "UIImageView+CGXExtension.h"
+#import "UIImageView+WebCache.h"
+#import "NSString+CGXExtension.h"
+#import "UIImage+CGXExtension.h"
 
 @implementation UIImageView (CGXExtension)
+
+- (void)setImageWithName:(NSString *)name {
+    self.image = [UIImage imageNamed:name];
+}
+
+- (void)setImageWithURL:(NSString *)url {
+    [self sd_setImageWithURL:url.URL];
+}
+
+- (void)setImageWithURL:(NSString *)url placeholderImage:(NSString *)placeholderImage {
+    [self sd_setImageWithURL:url.URL placeholderImage:placeholderImage.image];
+}
+
+- (void)setImageWithURL:(NSString *)url placeholderImage:(NSString *)placeholderImage completion:(void (^)(UIImage *, NSError *, NSURL *))completion {
+    [self sd_setImageWithURL:url.URL
+            placeholderImage:placeholderImage.image
+                   completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+                       if (completion) {
+                           completion(image, error, imageURL);
+                       }
+                   }];
+}
+
+- (void)startAnimatingWithGifImageName:(NSString *)gifImageName {
+    [self startAnimatingWithGifImageName:gifImageName repeatCount:0];
+}
+
+- (void)startAnimatingWithGifImageName:(NSString *)gifImageName repeatCount:(NSUInteger)repeatCount {
+    [self startAnimatingWithGifImagePath:[[NSBundle mainBundle] pathForResource:gifImageName ofType:@".gif"] repeatCount:repeatCount];
+}
+
+- (void)startAnimatingWithGifImagePath:(NSString *)gifImagePath {
+    [self startAnimatingWithGifImagePath:gifImagePath repeatCount:0];
+}
+
+- (void)startAnimatingWithGifImagePath:(NSString *)gifImagePath repeatCount:(NSUInteger)repeatCount {
+    UIGifImageObject *object = [UIGifImageObject gifImageObjectWithImagePath:gifImagePath];
+    [self startAnimatingWithImages:object.images duration:object.duration repeatCount:repeatCount];
+}
+
+- (void)startAnimatingWithImages:(NSArray<UIImage *> *)images duration:(CGFloat)duration {
+    [self startAnimatingWithImages:images duration:duration repeatCount:0];
+}
+
+- (void)startAnimatingWithImages:(NSArray<UIImage *> *)images duration:(CGFloat)duration repeatCount:(NSUInteger)repeatCount {
+    if (!images.count) {
+        return;
+    }
+    self.animationImages = images;
+    self.animationDuration = duration;
+    self.animationRepeatCount = repeatCount;
+    [self startAnimating];
+}
 
 @end
