f1093bd59accd7e1d986c2626c63af3311440509
曹冠雄 <1120643432@qq.com>
2017-12-16 16:33:08 +0800

    Update. Update sender tag method to button.tag.

--- CGXExtension/Vender/CustomIOSAlertView/CustomIOSAlertView.m
+++ CGXExtension/Vender/CustomIOSAlertView/CustomIOSAlertView.m
@@ -142,11 +142,11 @@ CGFloat buttonSpacerHeight = 0;
 - (IBAction)customIOS7dialogButtonTouchUpInside:(id)sender
 {
     if (delegate != NULL) {
-        [delegate customIOS7dialogButtonTouchUpInside:self clickedButtonAtIndex:[sender tag]];
+        [delegate customIOS7dialogButtonTouchUpInside:self clickedButtonAtIndex:((UIButton *)sender).tag];
     }
 
     if (onButtonTouchUpInside != NULL) {
-        onButtonTouchUpInside(self, (int)[sender tag]);
+        onButtonTouchUpInside(self, (int)((UIButton *)sender).tag);
     }
 }
 
